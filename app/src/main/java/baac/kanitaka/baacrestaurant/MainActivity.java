acba4fd7e3ee02ad3e5a984da718dd5ca2f65a09
kanitaka <kanitaka.ch@baac.or.th>
2015-10-19 14:17:20 +0700

    create SQLite success

--- app/src/main/java/baac/kanitaka/baacrestaurant/MainActivity.java
+++ app/src/main/java/baac/kanitaka/baacrestaurant/MainActivity.java
@@ -5,9 +5,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //explicit
+    private UserTABLE objUserTABLE;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Create and Connected DB
+        createAndConnected();
+    } //Main Method
+
+    private void createAndConnected() {
+        objUserTABLE = new UserTABLE(this);
     }
-}
+} //Main Class
