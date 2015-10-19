6f373e3310861d9bb6bcca68e4e3c3d458b05d1e
kanitaka <kanitaka.ch@baac.or.th>
2015-10-19 12:57:09 +0700

    เริ่มใช้ github

---
+++ app/src/main/java/baac/kanitaka/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,21 @@
+package baac.kanitaka.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by BAAC on 19/10/2015.
+ */
+public class MyOpenHelper {
+
+    //Explicit
+    private static final String DATABASE_NAME = "BAAC.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+
+
+
+    public MyOpenHelper(Context context) {
+    }   //constructor
+
+}   //Main Class
